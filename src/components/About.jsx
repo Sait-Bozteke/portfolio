a0c9723821bb8deb00252dc8ac1092fb9aeec4a5
Sait-Boz <sait.bozteke63@gmail.com>
2022-05-11 00:16:27 +0200

    file added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import Kendim from "../assets/kendim.jpg"
+import React from "react";
+import Kendim from "../assets/kendim.jpg";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-      <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-5'>
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-5">
           {/* <div className='sm:text-right pb-8 pl-4'>
             <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-center'>
               About Me
             </p>
           </div> */}
-         
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-5 px-4">
+          <div>
+            <img src={Kendim} alt="" className="max-w-[400px]" />
           </div>
-          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-5 px-4'>
-            <div><img src={Kendim} alt="" className='max-w-[400px]' /></div>
-            <div> <div className='sm:text-left text-4xl font-bold'>
+          <div>
+            {" "}
+            <div className="sm:text-left text-4xl font-bold">
               <p>Hi. I'm Sait</p>
             </div>
-            <div><p> A curious full-blown developer with strong creative thinking skills,
-            high energy, and integrity, skilled in developing complex solutions,
-            interested in writing new code. Has the ability to create effective
-            algorithms, interact positively, and communicate appropriately with
-            team members. Able to grasp new technologies and concepts quickly to
-            develop innovative and creative solutions to problems. Always eager
-            to learn about various technologies, tools, and libraries.</p>  </div>
-              
+            <div>
+              <p>
+                A curious full-blown developer with strong creative thinking
+                skills, high energy, and integrity, skilled in developing
+                complex solutions, interested in writing new code. Has the
+                ability to communicate appropriately with team members and
+                always eager to learn about various technologies, tools, and
+                libraries.
+              </p>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
