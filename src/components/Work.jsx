83fee52eb62e4a30a773bc3d2b99fbd032b0b323
Sait-Boz <sait.bozteke63@gmail.com>
2022-05-14 11:00:42 +0200

    file added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -31,7 +31,7 @@ const Work = () => {
 
             {/* Grid Item */}
           <div
-            style={{ backgroundImage: `url(${Contact})` }}
+            style={{ backgroundImage: `url(${Movie})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
@@ -54,14 +54,14 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${Movie})` }}
+            style={{ backgroundImage: `url(${Contact})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
-              {/* <span className='text-2xl font-bold text-white tracking-wider'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
-              </span> */}
+              </span>
               <div className='pt-8 text-center'>
                 <a href='https://movie-iota-gold.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
