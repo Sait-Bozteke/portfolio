f984293ab5bba05dbcaf08935281983333aed3e2
Sait-Boz <sait.bozteke63@gmail.com>
2022-05-15 16:32:26 +0200

    file added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,9 +21,9 @@ const Work = () => {
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
-            Projects
+            React JS Projects
           </p>
-          <p className='py-6'>// Check out some of my recent work</p>
+      
         </div>
 
 {/* Container */}
@@ -39,15 +39,15 @@ const Work = () => {
               <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
               </span>
-              <div className='pt-8 text-center'>
-                <a href='https://firebase-contact-app.vercel.app/'>
+              <div className='pt-8 text-center border-4'>
+                <a href='https://movie-iota-gold.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                   
+                   Demo
                   </button>
                 </a>
-                <a href='https://firebase-contact-app.vercel.app/'>
+                <a href='https://movie-iota-gold.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                
+                  Code
                   </button>
                 </a>
               </div>
@@ -63,13 +63,13 @@ const Work = () => {
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='https://movie-iota-gold.vercel.app/'>
+                <a href='https://firebase-contact-app.vercel.app/'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://movie-iota-gold.vercel.app/'>
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                <a href='https://firebase-contact-app.vercel.app/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white  text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
